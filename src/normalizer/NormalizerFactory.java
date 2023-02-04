ece17318b7c68a006f6faa3e0736963c58dfdc82
xxan15 <xxan15@vt.edu>
2023-02-04 17:18:06 -0500

    Removed NormalizerObjdump.java

--- src/normalizer/NormalizerFactory.java
+++ src/normalizer/NormalizerFactory.java
@@ -4,18 +4,18 @@ import java.io.FileNotFoundException;
 
 public class NormalizerFactory {
 
-	String asm_path;
-	String disasm_type;
+	String asmPath;
+	String disasmType;
 
-	public NormalizerFactory(String asm_path, String disasm_type) {
-        this.asm_path = asm_path;
-        this.disasm_type = disasm_type;
+	public NormalizerFactory(String asm_path, String disasmType) {
+        this.asmPath = asm_path;
+        this.disasmType = disasmType;
 	}
 
 
     public Normalizer get_disasm() throws FileNotFoundException {
-        if(this.disasm_type == "objdump")
-            return new NormalizeObjdump(asm_path);
+        if(this.disasmType == "idapro")
+            return new NormalizerIDAPro(asmPath);
         return null;
     }
 
